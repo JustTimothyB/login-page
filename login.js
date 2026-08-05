9a416da265ad87c720523ebb6a46504da5cf3f5d
JustTimothyB <106408706+JustTimothyB@users.noreply.github.com>
2026-08-05 16:58:01 -0400

    Added basic form login validation
    - added JS event listener to the login button.
    - prevented default form submissions.
    - reads email and password input values.
    - added basic validation for empty fields.
    - display validation messages on the form.
    - clear previous error messages before validating.

--- login.js
+++ login.js
@@ -1,5 +1,31 @@
 const loginButton = document.querySelector('.login-button');
+const emailInput = document.querySelector('.email-input');
+const passwordInput = document.querySelector('.password-input');
+const errorMessage = document.querySelector('.error-message')
 
 loginButton.addEventListener('click', (event) => {
-    console.log("login clicked");
+    event.preventDefault();
+
+    const email = emailInput.value;
+    const password = passwordInput.value;
+
+    if (email=== "") {
+        errorMessage.textContent = "Please enter your Email.";
+    }
+
+    else if (password=== ""){
+        errorMessage.textContent = "Please enter your Password.";
+    }
+
+    else {
+    errorMessage.textContent = "Login Successful";
+    }
+
+    if (email !== "" && password !==""){
+        console.log("There is an email");
+
+}
+
+
+
 });
